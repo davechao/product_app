52e27445584fa08e8695054e1563e6b6846af6f8
dave_chao <dave790602@gmail.com>
2019-01-13 11:21:14 +0800

    add drawer

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -5,6 +5,20 @@ class Products extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("Products"),
       ),
