6197aa6fdf8dee3aa2f76b9b73d4adb43561fcce
dave_chao <dave790602@gmail.com>
2019-01-13 22:31:06 +0800

    add showing modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create a product'),
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Center(
+                child: Text('This is a Modal!'),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
