92440ea046ccc3c2de39227ddc4dd368d3d554de
dave_chao <dave790602@gmail.com>
2019-01-28 00:08:21 +0800

    auth page add MediaQuery

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -77,6 +77,9 @@ class _AuthState extends State<Auth> {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
@@ -88,24 +91,27 @@ class _AuthState extends State<Auth> {
         padding: EdgeInsets.all(10.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                _buildEmailTextField(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                _buildPasswordTextField(),
-                _buildAcceptSwitch(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                RaisedButton(
-                  child: Text('LOGIN'),
-                  color: Colors.deepOrange,
-                  textColor: Colors.white,
-                  onPressed: _submitForm,
-                ),
-              ],
+            child: Container(
+              width: targetWidth,
+              child: Column(
+                children: <Widget>[
+                  _buildEmailTextField(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  _buildPasswordTextField(),
+                  _buildAcceptSwitch(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  RaisedButton(
+                    child: Text('LOGIN'),
+                    color: Colors.deepOrange,
+                    textColor: Colors.white,
+                    onPressed: _submitForm,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
