826d0bf98112a834022953b95e29a93645b15d04
dave_chao <dave790602@gmail.com>
2019-02-23 14:01:44 +0800

    setup map configuration for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,10 @@ import 'package:product_app/pages/product_card_list.dart';
 import 'package:product_app/pages/products_admin.dart';
 import 'package:product_app/scoped_models/main_model.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:map_view/map_view.dart';
 
 void main() {
+  MapView.setApiKey('AIzaSyDFqHBKELFROGyjfMkXVGoQcGHB_tT3vrw');
   runApp(MyApp());
 }
 
