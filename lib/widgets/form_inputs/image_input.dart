5aa73e0b19fd82518f55c7bc0ebab741f40b1321
dave_chao <dave790602@gmail.com>
2019-03-01 21:17:54 +0800

    add image picker

--- lib/widgets/form_inputs/image_input.dart
+++ lib/widgets/form_inputs/image_input.dart
@@ -8,13 +8,46 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 150.0,
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Pick an Image',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10.0),
+              FlatButton(
+                textColor: Theme.of(context).accentColor,
+                child: Text('Use Camera'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                textColor: Theme.of(context).accentColor,
+                child: Text('Use Gallery'),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final buttonColor = Theme.of(context).accentColor;
     return Column(
       children: <Widget>[
         OutlineButton(
-          onPressed: () {},
+          onPressed: () {
+            _openImagePicker(context);
+          },
           borderSide: BorderSide(color: buttonColor, width: 2.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
