09a54819355a700b7f37ab1f1f580e601d9abe73
dave_chao <dave790602@gmail.com>
2019-03-10 23:16:54 +0800

    add animate hero for product card and product detail

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -88,33 +88,47 @@ class ProductDetail extends StatelessWidget {
         return Future.value(false);
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            FadeInImage(
-              image: NetworkImage(product.image),
-              height: 300.0,
-              fit: BoxFit.cover,
-              placeholder: AssetImage('assets/food.jpg'),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: TitleDefault(product.title),
-            ),
-            _buildAddressPriceRow(
-              product.location.address,
-              product.price,
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                product.description,
-                textAlign: TextAlign.center,
+//        appBar: AppBar(
+//          title: Text(product.title),
+//        ),
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 256.0,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(product.title),
+                background: Hero(
+                  tag: product.id,
+                  child: FadeInImage(
+                    image: NetworkImage(product.image),
+                    height: 300.0,
+                    fit: BoxFit.cover,
+                    placeholder: AssetImage('assets/food.jpg'),
+                  ),
+                ),
               ),
-            )
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  alignment: Alignment.center,
+                  child: TitleDefault(product.title),
+                ),
+                _buildAddressPriceRow(
+                  product.location.address,
+                  product.price,
+                ),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    product.description,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ]),
+            ),
           ],
         ),
         floatingActionButton: ProductFab(product),
--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -65,11 +65,14 @@ class ProductCard extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          FadeInImage(
-            image: NetworkImage(product.image),
-            height: 300.0,
-            fit: BoxFit.cover,
-            placeholder: AssetImage('assets/food.jpg'),
+          Hero(
+            tag: product.id,
+            child: FadeInImage(
+              image: NetworkImage(product.image),
+              height: 300.0,
+              fit: BoxFit.cover,
+              placeholder: AssetImage('assets/food.jpg'),
+            ),
           ),
           _buildTitlePriceRow(),
           AddressTag(product.location.address),
