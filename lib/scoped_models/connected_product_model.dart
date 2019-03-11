415be8c3c51836bcfe42761820f9ffc214c5e6ff
dave_chao <dave790602@gmail.com>
2019-03-11 21:49:36 +0800

    fixed bug for logout

--- lib/scoped_models/connected_product_model.dart
+++ lib/scoped_models/connected_product_model.dart
@@ -128,6 +128,7 @@ mixin UserModel on ConnectedProductModel {
     _authenticatedUser = null;
     _authTimer.cancel();
     _userSubject.add(false);
+    _selProductId = null;
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove("token");
     prefs.remove("userEmail");
