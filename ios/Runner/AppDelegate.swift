5942969b1b2603b07ceb6c4f741a521d5064dad1
dave_chao <dave790602@gmail.com>
2019-03-16 15:11:42 +0800

    implenet getBatteryLevel for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,30 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let batteryChannel = FlutterMethodChannel(name: "samples.flutter.io/battery",
+                                              binaryMessenger: controller)
+    batteryChannel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        guard call.method == "getBatteryLevel" else {
+        result(FlutterMethodNotImplemented)
+            return
+        }
+        self?.receiveBatteryLevel(result: result)
+    });
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+private func receiveBatteryLevel(result: FlutterResult) {
+  let device = UIDevice.current
+  device.isBatteryMonitoringEnabled = true
+  if device.batteryState == UIDeviceBatteryState.unknown {
+    result(FlutterError(code: "UNAVAILABLE",
+                        message: "Battery info unavailable",
+                        details: nil))
+  } else {
+    result(Int(device.batteryLevel * 100))
   }
 }
